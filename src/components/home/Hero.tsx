f0c177fd0886a6899ed9c7f1d6c6bab7468b426d
divyansh23-gupta <divyanshgupta231@gmail.com>
2026-07-10 21:35:31 +0530

    style: replace hero slide 4 background with clean diamond ring image

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,7 +5,7 @@ import { Link } from "@tanstack/react-router";
 import hero1 from "@/assets/hero-1.jpg";
 import hero2 from "@/assets/hero-2.jpg";
 import hero3 from "@/assets/hero-3.jpg";
-import hero4 from "@/assets/hero-4.png";
+import hero4 from "@/assets/hero-4.jpg";
 
 
 interface Slide {
